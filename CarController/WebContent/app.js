1c0b6da6b516b98503dd362816949aaf2d2f417a
Fabio Tessaro <fabio.tessaro.porta@gmail.com>
2018-07-14 15:37:10 +0200

    Eliminazione vecchio progetto

--- CarController/WebContent/app.js
+++
@@ -1,6 +0,0 @@
-var app = angular.module("LegoCarApp", ["ngMaterial","ngMessages","ngAnimate","ngSanitize","ngAria","LocalStorageModule", "hmTouchEvents"]);
-
-app.config(function(localStorageServiceProvider){
-	localStorageServiceProvider.setPrefix('LegoCarApp');
-	localStorageServiceProvider.setDefaultToCookie(false);
-});
