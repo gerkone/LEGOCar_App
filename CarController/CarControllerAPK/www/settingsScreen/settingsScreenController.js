088611693430172748d886165c8f2f57bbaef596
ggalletti99 <gianlucagalletti@ymail.com>
2018-07-15 02:53:42 +0200

    fatti male

--- CarController/CarControllerAPK/www/settingsScreen/settingsScreenController.js
+++ CarController/CarControllerAPK/www/settingsScreen/settingsScreenController.js
@@ -12,4 +12,27 @@ function settingsScreenController($scope, $timeout, localStorageService) {
 		console.log("starting!");
 	}
 	
+	$scope.openMenu = function($mdMenu, ev) {
+	      originatorEv = ev;
+	      $mdMenu.open(ev);
+	    };
+	    
+	$scope.scanWiFi = function() {
+		WifiWizard.getScanResults({numLevels: 5}, $scope.updateWiFiList(result), $scope.updateWiFiList(false));
+	}    
+	
+	$scope.updateWiFiList = function(result) {
+		if(()!result) || (result==null)) {
+			//TODO show simple tost mostrando errore
+		} else {
+			for(i = 0; i < result.lenght; i++) {
+				$scope.availableNetworks[i] = result[i];
+			}
+		}
+	}
+	
+	$scope.showDefaults = function() {
+		
+	}
+	
 }
